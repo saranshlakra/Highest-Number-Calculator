cdecfaff036b2f8c4bc20cc85f056ad6224e2ecc
Saransh Lakra <saranshlakra4@gmail.com>
2020-01-09 01:38:33 +0530

    Update HighestnoCalci.js

--- HighestnoCalci.js
+++ HighestnoCalci.js
@@ -1,14 +1,12 @@
 // highest number calcultor
 var numbers = [9, 2, 4, 60, 8, 10, 100, 12];
 var numMax1 = numbers[0]
-
 for (i = 0; i < numbers.length; i++) {
     //  console.log(numbers[i]);
     if (numbers[i] >= numMax1) {
         numMax1 = numbers[i];
     } else {
-
-    }
+  // console.log('Hello World');}
 }
 console.log('Highest Number: ' + numMax1);
 console.log(numbers);
