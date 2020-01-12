83be08685b82caaae28101f0ff04026f06907308
saranshlakra <saranshlakra4@gmail.com>
2020-01-12 14:06:24 +0530

    1st and 2nd highest numbers
    Now you can also find 2nd highest number also.

--- HighestnoCalci.js
+++ HighestnoCalci.js
@@ -1,14 +1,23 @@
 // highest number calcultor
-var numbers = [9, 2, 4, 60, 8, 10, 100, 12];
-var numMax1 = numbers[0]
+var numMax1, numMax2, numMax3;
+var numbers = [9, 2, 4, 60, 80, 10, 100, 12];
+numMax1 = numbers[0]
+numMax2 = numbers[0]
+numMax3 = numbers[0]
 
 for (i = 0; i < numbers.length; i++) {
     //  console.log(numbers[i]);
     if (numbers[i] >= numMax1) {
+        numMax3 = numMax2;
+        numMax2 = numMax1;
         numMax1 = numbers[i];
+    } else if (numbers[i] >= numMax2) {
+        numMax3 = numMax2;
+        numMax2 = numbers[i];
     } else {
-
+        numMax3 = numbers[i];
     }
 }
-console.log('Highest Number: ' + numMax1);
+
 console.log(numbers);
+console.log('1st Highest Number: ' + numMax1 + ',' + '2nd Highest Number: ' + numMax2 + '.');
